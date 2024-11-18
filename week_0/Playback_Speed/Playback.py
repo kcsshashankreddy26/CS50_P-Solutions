6a6e2145d711d5cf699abb542d09a6b678de6c68
kcsshashankreddy26 <kcsshashankreddy26@gmail.com>
2024-11-18 07:59:22 +0530

    added Playback Speed Solution

---
+++ week_0/Playback_Speed/Playback.py
@@ -0,0 +1,10 @@
+#asking the user for input
+
+#using the str.replace() method to replace every space with three periods
+#.replace("old","new")
+#or
+#print(msg.replace(" ","...",5))
+#or
+#print(msg.replace(" ","..."))
+
+print(input("How are you gonna test the code? ").replace(" ","..."))
