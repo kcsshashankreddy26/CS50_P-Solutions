cf64c8a4abc0fa483ceecb7302e002d3aae4d229
kcsshashankreddy26 <kcsshashankreddy26@gmail.com>
2024-11-19 07:41:51 +0530

    Added einstein.py to the week_0

---
+++ week_0/einstein/einstein.py
@@ -0,0 +1,8 @@
+#asking the user for input
+m = int(input("Enter the mass : "))
+#initialising the variable c with its universal value
+c = 300000000
+E = m*(c**2)
+#printing the value so the formula is depicted
+print(E)
+
